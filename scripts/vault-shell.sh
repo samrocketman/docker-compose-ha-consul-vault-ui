00c817db0f9a5a59df6951d1d357423c5fe4ac22
Sam Gleske <sam.mxracer@gmail.com>
2020-08-19 23:20:40 -0400

    Add interactive vault shell script

---
+++ scripts/vault-shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# DESCRIPTION
+# Provides an interactive shell to type commands directly into Vault.  The
+# session is pre-authenticated with an admin token.
+
+source scripts/vault-functions.sh
+set_vault_addr
+set_vault_admin_token
+trap 'revoke_self' EXIT
+
+docker-compose exec -e VAULT_TOKEN -e VAULT_ADDR vault /bin/sh
