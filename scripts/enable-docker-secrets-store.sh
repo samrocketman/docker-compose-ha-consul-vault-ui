6c5f9607faf7b7af026473983427dca2044ec682
Sam Gleske <sam.mxracer@gmail.com>
2020-08-25 21:36:37 -0400

    Skip enabling docker secrets engine if enabled

--- scripts/enable-docker-secrets-store.sh
+++ scripts/enable-docker-secrets-store.sh
@@ -4,6 +4,10 @@ source scripts/vault-functions.sh
 
 set_vault_admin_token
 
+if execute_vault_command vault secrets list | grep '^docker/'; then
+  exit
+fi
+
 echo 'Enable KV v2 secrets engine for docker infra.'
 execute_vault_command vault secrets enable -path=docker/ -version=2 kv
 
