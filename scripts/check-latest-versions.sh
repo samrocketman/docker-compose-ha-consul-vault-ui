a8c03b18859753ede808c6126175fd20333693bb
Sam Gleske <sam.mxracer@gmail.com>
2020-08-27 01:06:47 -0400

    small refactor check-latest-versions

--- scripts/check-latest-versions.sh
+++ scripts/check-latest-versions.sh
@@ -2,6 +2,14 @@
 # Checks the current version of consul, consul-template, and vault.  It will
 # print the version of each.
 
+# check versions of the following software
+software=(
+  consul
+  consul-template
+  vault
+)
+
+# Reads a list of version numbers and prints the highest version number found.
 function get_highest_release() {
   awk -F. '
   {
@@ -28,12 +36,14 @@ function get_highest_release() {
   '
 }
 
-function getversions() {
+# Given a HashiCorp software, print the latest release
+function get_latest() {
   curl -s https://releases.hashicorp.com/"$1"/ |
     grep -o "$1"'/[.0-9]\+/' |
-    cut -d/ -f2
+    cut -d/ -f2 |
+    get_highest_release
 }
 
-for x in consul consul-template vault; do
-  echo "$x $(getversions "$x" | get_highest_release)"
+for x in "${software[@]}"; do
+  echo "$x $(get_latest "$x")"
 done
