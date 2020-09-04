e0e88775e37274d1c7cdb7d8087e6d2ad311bed8
Sam Gleske <sam.mxracer@gmail.com>
2020-09-04 19:13:40 -0400

    Update stop vault to use curl-api.sh for seal
    Only the active endpoint of the Vault cluster needs to seal the backend.

--- scripts/stop-vault.sh
+++ scripts/stop-vault.sh
@@ -2,19 +2,11 @@
 
 set -ex
 
-VAULT_TOKEN="$(awk '$0 ~ /Initial Root Token/ { print $NF }' secret.txt)"
-export VAULT_TOKEN
+source scripts/vault-functions.sh
+set_vault_admin_token 1m
 
-export run=(docker-compose exec -T)
-
-count=$("${run[@]}" consul consul catalog nodes -service=vault | wc -l)
-((count=count-1))
-
-run+=( -e VAULT_TOKEN="$VAULT_TOKEN" )
-
-for x in $(eval echo {1..$count}); do
-  "${run[@]}" --index="$x" vault vault operator step-down || true
-  "${run[@]}" --index="$x" vault vault operator seal || true
-done
+./scripts/curl-api.sh \
+  --request PUT \
+  http://active.vault.service.consul:8200/v1/sys/seal
 
 docker-compose stop vault
